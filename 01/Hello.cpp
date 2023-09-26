cfd53fbc82fe1ad7f04067d17a1b579fabcbc8ab
Mohammad SN <shahnazi2002@gmail.com>
2023-09-26 18:14:34 +0330

    Create Hello.cpp

---
+++ 01/Hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Please enter your name:";
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+    return 0;
+}
