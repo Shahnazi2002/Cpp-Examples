574514c9d6d4e89f04fbd8a454a06e61a52121e3
Mohammad SN <shahnazi2002@gmail.com>
2023-09-29 18:34:51 +0330

    Create floatproduct.cpp

---
+++ 05/floatproduct.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "This program calculates product of two floating-point numbers." << endl;
+
+    float x, y;
+
+    cout << "Enter the first number: ";
+    cin >> x;
+    cout << "Enter the second number: ";
+    cin >> y;
+
+    cout << "Result: " << x*y << endl;
+    return 0;
+}
