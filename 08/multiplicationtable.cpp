0d62bd57134f29cf6318fa47ccf2d524d57dbc6a
Mohammad SN <shahnazi2002@gmail.com>
2023-10-01 09:18:34 +0330

    Create multiplicationtable.cpp

---
+++ 08/multiplicationtable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << " - - - - - - - - - - - - - Multiplication Table - - - - - - - - - - - - - " << endl;
+    for (int i = 1; i<=10; i++) {
+        for (int j = 1; j<=10; j++) {
+            cout << i*j << '\t';
+        }
+        cout << '\n' << '\n';
+    }
+    return 0;
+}
